3d00196136e821afca9df5c5c12e1f281ce85a34
DC00 <djc4ku@virginia.edu>
2017-11-28 08:27:31 -0500

    first attempt at mongoose connection

--- reddit-ssbm.js
+++ reddit-ssbm.js
@@ -6,7 +6,9 @@
 
 const puppeteer = require('puppeteer');
 const cheerio = require('cheerio');
-const mongo = require('mongoose');
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+const mongo = require('mongodb').MongoClient;
 const CREDS = require('./creds');
 const URL = "https://www.reddit.com/r/smashbros/wiki/events"
 const DB_URL = "mongodb://" + CREDS.username + ":" + CREDS.password + "@ds121456.mlab.com:21456/ecalo"
@@ -41,10 +43,34 @@ async function run(){
 };
 
 function upsert(data) {
-	if(mongoose.connection.readyState == 0) {
-		mongoose.connect(DB_URL);
+	mongoose.connection.on("connected", function(ref) {
+		console.log("connected");
+	});
+
+	mongoose.connection.on("error", function(err) {
+		console.log(err);
+	});
+
+	mongoose.connection.on("disconnected", function() {
+		console.log("disconnected");
+	});
+
+	var gracefulExit = function() {
+		mongoose.connection.close(function () {
+			console.log("disconnected from node termination");
+			process.exit(0);
+		});
 	}
 
-};
+	process.on('SIGINT', gracefulExit).on('SIGTERM', gracefulExit);
+
+	try {
+		// options.server.socketOptions = options.replset.socketOptions = { keepAlive: 1 };
+		mongoose.connect(DB_URL);
+		console.log("Trying to connect to DB");
+	} catch (err) {
+		console.log("Sever initialization failed " , err.message);
+	}
+}
 
 run();
